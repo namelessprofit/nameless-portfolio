85601a1e9aef2c489dd3937871ab8b2130f80c33
namelessprofit <posadaarchy@gmail.com>
2017-02-23 23:50:49 -0800

    added text in text in modals for name and email. testing functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,24 @@
 // sanity check
-console.log("app.js linked.");
-function myFunction() {
-    document.getElementById("myForm").submit();
-}
+// console.log("app.js linked.");
+// function myFunction() {
+//     document.getElementById("myForm").submit();
+// }
+submitBtn.click(function () {
+    if ($("#valueText").val() === "ENTER VALUE")
+    {
+        alert("please insert a valid value");
+        return false;
+    }
+});
+
+valueText.blur(function () {
+    if(this.value == ''){
+        this.value = 'ENTER VALUE';
+    }
+});
+
+ valueText.focus(function () {
+    if (this.value == 'ENTER VALUE') {
+        this.value = '';
+    }
+});
