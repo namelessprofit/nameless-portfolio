06c38f3ef3ca82efd20a41f790b2a05eff1c453b
namelessprofit <posadaarchy@gmail.com>
2017-02-24 00:58:16 -0800

    fixed submit form function, still need to find a way to store the info. Also added dissappearing text in modals but currently having issues with the js code. text is not dissappearing when user clicks in modal. added second photo for bottom of page.

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,22 +3,22 @@ console.log("app.js linked.");
 function myFunction() {
     document.getElementById("myForm").submit();
 }
-submitBtn.click(function () {
-    if ($("#valueText").val() === "ENTER VALUE")
-    {
-        alert("please insert a valid value");
-        return false;
-    }
-});
-
-valueText.blur(function () {
-    if(this.value == ''){
-        this.value = 'ENTER VALUE';
-    }
-});
-
- valueText.focus(function () {
-    if (this.value == 'ENTER VALUE') {
-        this.value = '';
-    }
-});
+// submitBtn.click(function () {
+//     if ($("#valueText").val() === "ENTER VALUE")
+//     {
+//         alert("please insert a valid value");
+//         return false;
+//     }
+// });
+//
+// valueText.blur(function () {
+//     if(this.value == ''){
+//         this.value = 'ENTER VALUE';
+//     }
+// });
+//
+//  valueText.focus(function () {
+//     if (this.value == 'ENTER VALUE') {
+//         this.value = '';
+//     }
+// });
